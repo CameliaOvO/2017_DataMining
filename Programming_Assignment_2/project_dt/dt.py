45e64e30251e5dff97615a6e453497216e6d4e3a
Camelia <camelia0858@gmail.com>
2017-04-10 12:29:38 +0900

    start decision tree

---
+++ Programming_Assignment_2/project_dt/dt.py
@@ -0,0 +1,11 @@
+# Data Science Programming Assignment 2 : decision tree
+# author : Seonha Park
+# written in Python3
+
+import sys
+
+#get command line argument
+train_file = sys.argv[1]
+test_file = sys.argv[2]
+output_file = sys.argv[3]
+
