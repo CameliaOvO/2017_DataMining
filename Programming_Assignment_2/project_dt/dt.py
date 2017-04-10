8b1ed596843b21653ae79b9ad55f3b5cc475a424
Camelia <camelia0858@gmail.com>
2017-04-10 19:38:38 +0900

    make D_1

--- Programming_Assignment_2/project_dt/dt.py
+++ Programming_Assignment_2/project_dt/dt.py
@@ -4,8 +4,55 @@
 
 import sys
 
-#get command line argument
-train_file = sys.argv[1]
-test_file = sys.argv[2]
-output_file = sys.argv[3]
+# TODO: implement attribute selection method(D, attr_list) in 3 ways
+def attr_selection(D, attribute_list):
+    attr_name = ""
+    return attr_name
+
+
+
+
+# get command line argument
+# train_file = sys.argv[1]
+# test_file = sys.argv[2]
+# output_file = sys.argv[3]
+
+# erase after finish dt
+train_file = "data/dt_train1.txt"
+test_file = "data/dt_test1.txt"
+output_file = "data/dt_result.txt"
+
+# get train file and make list of columens
+with open(train_file) as f:
+    train_data = f.readlines()
+train_data = [d.strip() for d in train_data]
+
+# make train_data string to attribute list and get each attribute name and total number of column
+attribute_list = []
+total_attribute = 0
+for line in train_data:
+    each_line = line.split("\t")
+    attribute_list.append(each_line)
+    total_attribute += 1
+attribute_name = attribute_list[0]
+attribute_list.pop(0)
+total_attribute -= 1
+
+
+# make D_1
+data_partition = {}
+for i in range(len(attribute_name)):
+    ith_attr = {}
+    for attr in attribute_list:
+        if attr[i] not in ith_attr:
+            ith_attr[attr[i]] = 1
+        else :
+            ith_attr[attr[i]] += 1
+    data_partition[attribute_name[i]] = ith_attr
+
+
+for k, v in data_partition.items():
+    print(k),
+    print(v)
+
 
