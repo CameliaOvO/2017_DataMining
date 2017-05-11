7a21c87c1d693147e9567cf5ea40644684050180
Camelia <camelia0858@gmail.com>
2017-05-12 02:53:20 +0900

    implemented regionQuery

--- Programming_Assignment_3/project_clustering/clustering.py
+++ Programming_Assignment_3/project_clustering/clustering.py
@@ -3,17 +3,29 @@
 # written in Python3
 
 import sys
+import math
 
-#get command line argument
-# input_file = sys.argv[1]
-# num_of_cluster = sys.argv[2]
-# eps = sys.argv[3]
-# minpts = sys.argv[4]
+# get distance between p[idx1, x1, y1] and q[idx1, x2, y2]
+def dist(p,q):
+    return math.sqrt(abs(p[1]-q[1])**2 + abs(p[2]-q[2])**2)
 
-input_file = "data/input1.txt"
-num_of_cluster = 8
-eps = 15
-minpts = 22
+# use in regionQuery(P,eps)
+def checkEpsNeighbor(p,q,eps):
+    return dist(p,q) < eps
+
+# return all points within P's eps-neighborhood (including P)
+def regionQuery(p,all_point,eps):
+    returnPts = []
+    for q in all_point:
+        if checkEpsNeighbor(p,q,eps):
+            returnPts.append(q)
+    return returnPts
+
+def DBSCAN(D, eps, minPts):
+    pass
+
+def expandCluster(P,N,C,eps,minPts):
+    pass
 
 """
 DBSCAN(D, eps, MinPts)
@@ -38,6 +50,32 @@ expandCluster(P,N,C,eps,MinPts)
       if P' is not yet member of any cluster
          add P' to cluster C
 
-regionQuery(P, eps)
-   return all points within P's eps-neighborhood (including P)
 """
+
+def main():
+    # get command line argument
+    # input_file = sys.argv[1]
+    # num_of_cluster = sys.argv[2]
+    # eps = sys.argv[3]
+    # minpts = sys.argv[4]
+
+    input_file = "data/input1.txt"
+    num_of_cluster = 8
+    eps = 15
+    minpts = 22
+
+    with open(input_file) as f:
+        input_data = f.readlines()
+    input_data = [d.strip() for d in input_data]
+
+    # make train_data string to attribute list and get each attribute name and total number of columns
+    object_list = []
+    total_object = 0
+    for line in input_data:
+        each_line = line.split("\t")
+        object_list.append(each_line)
+        total_object += 1
+
+
+if __name__ == '__main__':
+    main()
